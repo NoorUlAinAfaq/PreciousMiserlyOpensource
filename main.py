1d3b077003deab817dda69fec73a6633491802cf
Noor Ul Ain Afaq <59082791+NoorUlAinAfaq@users.noreply.github.com>
2024-11-19 15:57:12 +0000

    Quiz App

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-import time
+# import time
 
-# timestamp = time.strftime("%H:%M:%S")
-print(time.clock_gettime(9))
-print(time.strftime("%H:%M:%S"))
+# # timestamp = time.strftime("%H:%M:%S")
+# print(time.clock_gettime(9))
+# print(time.strftime("%H:%M:%S"))
+
+quiz = [
+  {
+      "question": "What is the capital of France?",
+      "options": ["A) Paris", "B) Madrid", "C) Berlin", "D) Rome"],
+      "correct_answer": "A",
+  },
+  {
+      "question": "What is 2 + 2?",
+      "options": ["A) 3", "B) 4", "C) 5", "D) 6"],
+      "correct_answer": "B",
+  },
+]
+
+# Accessing a question
+for item in quiz:
+  print(item["question"])
+  print("\n".join(item["options"])) 
+  user_answer = input("Enter your answer (A, B, C, or D): ")
+
+  if user_answer.upper() == item["correct_answer"]:
+    print("Correct!")
+  else:
+    print("Wrong")
+    print("\n Correct answer is:" + item["correct_answer"])
