ff1d1a8f3bd54d34dceed712845b82be36c34ea9
Noor Ul Ain Afaq <59082791+NoorUlAinAfaq@users.noreply.github.com>
2024-11-28 10:24:35 +0000

    Sets and excepetio handling

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+set1 = {2, 4, 65, 'u'}
+set2 = {3, 7, 6}
+set3 = set()
+print(set1.union(set2), set3)
+print(set1.intersection(set2))
+print(set1.difference(set2))
+
+set3.add(2)
+print(hash(frozenset(set2)))
+print(set3)
+print(hash("noorafaq"))
+
+a = input("Enter the number:")
+
+try:
+    for index in range(1, 11):
+        print(f"{int(a)} X {index} = {int(a)*index}")
+except ValueError:
+    print("Invalid Input")
 # # import time
 
 # # # timestamp = time.strftime("%H:%M:%S")
@@ -44,30 +63,30 @@
 # print(f"x = {x}")            
 # print(f"{x = }")        
 
-def fibonacci_iterative(n):
-    """Prints the first n Fibonacci numbers using an iterative approach."""
-    # Handle edge cases
-    if n <= 0:
-        print("Please enter a positive integer.")
-        return
+# def fibonacci_iterative(n):
+#     """Prints the first n Fibonacci numbers using an iterative approach."""
+#     # Handle edge cases
+#     if n <= 0:
+#         print("Please enter a positive integer.")
+#         return
 
-    # Base cases
-    if n == 1:
-        print("0")
-        return
-    elif n == 2:
-        print("0, 1")
-        return
+#     # Base cases
+#     if n == 1:
+#         print("0")
+#         return
+#     elif n == 2:
+#         print("0, 1")
+#         return
 
-    # Initialize the first two Fibonacci numbers
-    a, b = 0, 1
-    print(a,"\n",b)
+#     # Initialize the first two Fibonacci numbers
+#     a, b = 0, 1
+#     print(a,"\n",b)
 
-    # Generate the rest of the Fibonacci sequence
-    for _ in range(n - 2):
-        c = a + b
-        print(c)
-        a, b = b, c
+#     # Generate the rest of the Fibonacci sequence
+#     for _ in range(n - 2):
+#         c = a + b
+#         print(c)
+#         a, b = b, c
 
-# Example usage
-fibonacci_iterative(7)
+# # Example usage
+# fibonacci_iterative(7)
